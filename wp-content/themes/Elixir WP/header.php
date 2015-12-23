081c83ae7e282b58e7bb08d8547e6a8503192bd5
foresitegroup <mark@foresitegrp.com>
2015-12-23 10:51:39 -0600

    Add Google Analytics

--- wp-content/themes/Elixir WP/header.php
+++ wp-content/themes/Elixir WP/header.php
@@ -12,6 +12,17 @@
         <script src="./js/respond.js"></script>
     <![endif]-->
     <?php wp_head(); ?>
+    <!-- BEGIN Google Analytics -->
+    <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+     ga('create', 'UA-71713050-1', 'auto');
+     ga('send', 'pageview');
+    </script>
+    <!-- END Google Analytics -->
 </head>
 <body <?php body_class($jellythemes['color']); ?>>
     <div id="mask">
